fa5877c729a4295d35dd7bbd345cffa3ebf55d50
MarcoDalmasso04 <91015879+MarcoDalmasso04@users.noreply.github.com>
2021-10-01 09:38:45 +0200

    Rename main.c to es1.c

--- es1.c
+++ es1.c
@@ -8,9 +8,9 @@ ESERCIZIO 1: Inserimento ordinato
 
 Si scriva un programma che:
 
--Definisca un tipo di dato STUDENTE. Ogni studente è caratterizzato da un nome, cognme e una matricola.
+-Definisca un tipo di dato STUDENTE. Ogni studente Ã¨ caratterizzato da un nome, cognme e una matricola.
 
--Acqusisca i dati di 10 studenti e, per ogni nuovo studente inserito, proceda ad un inserimento ordinato (lo scopo dell'esercizio è
+-Acqusisca i dati di 10 studenti e, per ogni nuovo studente inserito, proceda ad un inserimento ordinato (lo scopo dell'esercizio Ã¨
  ordinare gli studenti durante il processo di inserimento e non dopo con un algoritmo di ordinamento
 
 -Stampi i dati degli studenti.
